11286409521121a6a5a2e31ad61cb1f787cd20ad
Viviane Gomes Araujo <viviane.sp13@gmail.com>
2021-03-22 18:27:58 -0300

    First Commit

---
+++ primeiroCodigo/index.js
@@ -0,0 +1,16 @@
+// /*
+//     var nome = "Viviane"
+//     var idade = 21
+//     alert(nome + " " + idade)
+// */
+// var precoDolar = parseFloat(prompt("Digite um valor em dólar para conversão: "))
+
+// var precoReal = (precoDolar * 5.5).toFixed(2)
+
+// alert("R$"+precoReal)
+
+var catchPreco=parseFloat("");
+function getPrecoDolar () {
+    catchPreco = parseFloat((document.getElementById('precoDolar').value)*5.5.toFixed(2));
+    document.getElementById('precoReal').innerHTML = parseFloat(catchPreco);
+}
